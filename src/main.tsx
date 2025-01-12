853129083655bd228403e8ad97d28770ae0d32c7
a96363877 <a96363877@gmail.com>
2025-01-12 11:15:58 -0800

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,29 @@
+import { StrictMode, useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+const AppMain = () => {
+  const [total, setTotal] = useState(0);
+  const [ip, setIp] = useState('');
+
+  const [cart, setCart] = useState([{ id: 0, name: '', price: '', img: '' }]);
+
+  const addToCart = (item: any) => {
+    setCart([item, ...cart]);
+    setTotal((total) => total + parseInt(item.price));
+  };
+  return (
+    <App
+      cart={cart}
+      addToCart={addToCart}
+      total={total}
+      setIp={setIp}
+      ip={ip}
+    />
+  );
+};
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <AppMain />
+  </StrictMode>
+);
