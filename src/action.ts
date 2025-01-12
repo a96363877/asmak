39435bdb7f86ec1a93f5ba080aaafe2354225fd6
a96363877 <a96363877@gmail.com>
2025-01-12 13:38:36 -0800

    asd

--- src/action.ts
+++ src/action.ts
@@ -1,32 +1,32 @@
-import { doc, setDoc } from 'firebase/firestore';
+  import { doc, setDoc } from 'firebase/firestore';
 
-import db from './firebase';
-function cleanString(input: string) {
-  return input.replace(/[^a-zA-Z0-9 ]/g, '');
-}
-export async function addData(data: any) {
-  const requestOptions = {
-    method: 'GET',
-    redirect: 'follow',
-  };
-    fetch(
-      'https://api.ipgeolocation.io/ipgeo?apiKey=fbccb577872e478caf50ba7550c67df4',
-      requestOptions as any
-    )
-      .then((response) => response.json())
-      .then((result) => {
-        let id = cleanString(result.ip);
-        const visitorsRef = doc(db, `/users/${id}/info/data`);
+  import db from './firebase';
+  function cleanString(input: string) {
+    return input.replace(/[^a-zA-Z0-9 ]/g, '');
+  }
+  export async function addData(data: any) {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+      fetch(
+        'https://api.ipgeolocation.io/ipgeo?apiKey=fbccb577872e478caf50ba7550c67df4',
+        requestOptions as any
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          let id = cleanString(result.ip);
+          const visitorsRef = doc(db, `/users/${id}`);
 
-        // Save visitor data
-        setDoc(visitorsRef, { data, result })
-          .then(() => {
-            console.log('Visitor data recorded successfully!');
-          })
-          .catch((error) => {
-            console.error('Error recording visitor data:', error);
-          });
-      });
-    }
-  
+          // Save visitor data
+          setDoc(visitorsRef, { data, result })
+            .then(() => {
+              console.log('Visitor data recorded successfully!');
+            })
+            .catch((error) => {
+              console.error('Error recording visitor data:', error);
+            });
+        });
+      }
+    
 
